cbae57966ae4f1503e15c7aabeee1b740137f285
Abdul Kadir <aq8020588@gmail.com>
2026-08-31 18:21:25 +0530

    Test clear (Passout with 80%)Update in cardetail page

--- src/Pages/CarDetailsPage.jsx
+++ src/Pages/CarDetailsPage.jsx
@@ -33,7 +33,48 @@ export default function CarDetailsPage() {
     <>
       <Breadcrum title={data.name ?? "Car Details"} />
 
-      <div className="container-fluid">
+      <div className="container-fluid my-5">
+        <div className="row mb-3">
+          <div className="col-md-6"></div>
+          <div className="col-md-6">
+            <h5 className='bg-primary p-2 text-light text-center rounded-top'>{data.name}</h5>
+
+            <table className='table table-bordered text-dark '>
+              <tr>
+                <th>Category</th>
+                <td>{data.category}</td>
+              </tr>
+              <tr>
+                <th>Brand</th>
+                <td>{data.brand}</td>
+              </tr>
+              <tr>
+                <th>Registration Number</th>
+                <td>{data.registrationNumber}</td>
+              </tr>
+              <tr>
+                <th>Driving Mode</th>
+                <td>{data.drivingMode}</td>
+              </tr>
+              <tr>
+                <th>Driver</th>
+                <td>{data.driver ? "With Driver" : "Without Driver"}</td>
+              </tr>
+              <tr>
+                <th>Fuel Type</th>
+                <td>{data.type}</td>
+              </tr>
+              <tr>
+                <th>Seating Capacity</th>
+                <td>{data.seatingCapacity}</td>
+              </tr>
+              <tr>
+                <th>Rent</th>
+                <td><del className='text-primary'>&#8377;{data.baseRentAmount}</del> &#8377;{data.finalRentAmount} <sup>{data.discount}% off</sup></td>
+              </tr>
+            </table>
+          </div>
+        </div>
         <CarSlider similarCar={similarCar} />
       </div>
     </>
